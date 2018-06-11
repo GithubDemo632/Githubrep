b9e4137bb04f628d02961d717da6d5dd9f6ca235
SaaDev2 <SaaDev2@gmail.com>
2018-06-11 10:55:09 +0530

    commit:by Dev2

--- Githubproj/src/com/nt/tapan/App1.java
+++ Githubproj/src/com/nt/tapan/App1.java
@@ -3,6 +3,9 @@ package com.nt.tapan;
 public class App1 {
 	public static void main(String[] args) {
 		System.out.println("Hello123456");
+		for(i=0;i<10;i++){
+			System.out.println(i);
+		}
 	}
 //tapan kumar
 }
